1ac919506e659e16b2040b118d941ae385227c2c
Arturazambuja2002 <arturazambuja2002@gmai.com>
2020-10-18 23:28:29 -0200

    Mudança no console.log

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/',function(req,res){
-    res.send("Olá Artur Azambuja da Silva")
+    res.send("Olá Artur Azambuja da Silvagit")
 })
 
 app.listen(3000,function(){
-    console.log("Conexão inicializada na porta 3000")
+    console.log("Conexão inicializada")
 })
