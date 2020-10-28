91d4810edeb88ad2be17115e69b693230fac8879
Arturazambuja2002 <arturazambuja2002@gmail.com>
2020-10-28 10:56:49 -0200

    Criando mensagem

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 app.get('/',function(req,res){
     res.send("Olá Artur Azambuja da Silvagit")
 })
-
+app.get('/mensagem', function(req,res){
+    res.send("Mensagem enviada")
+})
 app.listen(3000,function(){
     console.log("Conexão inicializada")
 })
