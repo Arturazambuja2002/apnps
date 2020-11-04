03396dde2ed369a3b2a302f6903c17221b78baaa
Arturazambuja2002 <arturazambuja2002@gmail.com>
2020-11-03 22:14:25 -0200

    Configurações iniciais do projeto

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
     res.send("Olá Artur Azambuja da Silvagit")
 })
-app.get('/mensagem', function(req,res){
-    res.send("Mensagem enviada")
-})
+
 app.listen(3000,function(){
     console.log("Conexão inicializada")
 })
